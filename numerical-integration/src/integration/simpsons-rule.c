2c989f4691c7a24b3265f6fcfb119a5a560c4b37
ptrvsrg <s.petrov1@g.nsu.ru>
2023-11-01 22:28:06 +0700

    Fix algorithm

--- numerical-integration/src/integration/simpsons-rule.c
+++ numerical-integration/src/integration/simpsons-rule.c
@@ -1,17 +1,15 @@
 #include "integration/integration-methods.h"
 
 long double simpsons_rule(function func, long double a, long double b, int n) {
-    if (n % 2 == 1) {
-        n++;
-    }
-
-    long double h = (b - a) / n;
+    long double h = (b - a) / (2 * n);
     long double sum = func(a) + func(b);
 
-    for (int i = 1; i < n; i += 2) {
-        sum += 4 * func(a + i * h);
-        sum += 2 * func(a + (i + 1) * h);
+    for (int i = 1; i < n; i++) {
+        sum += 2 * func(a + (2 * i) * h);
+        sum += 4 * func(a + (2 * i + 1) * h);
     }
+
+    sum += 4 * func(a + h);
 
     return h * sum / 3;
 }
