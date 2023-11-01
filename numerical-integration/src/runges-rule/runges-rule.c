85fdcbc4c6de76bd827d979e2864593d1916cead
ptrvsrg <s.petrov1@g.nsu.ru>
2023-11-01 22:27:45 +0700

    If the differences are close to zero, I reset them manually

--- numerical-integration/src/runges-rule/runges-rule.c
+++ numerical-integration/src/runges-rule/runges-rule.c
@@ -7,5 +7,11 @@ long double runges_rule(integration_method integration_meth, function func, long
     long double I2 = integration_meth(func, a, b, 2*n);
     long double I3 = integration_meth(func, a, b, 4*n);
 
-    return log2l(fabsl((I1 - I2) / (I2 - I3)));
+    long double I12 = I1 - I2;
+    if (fabsl(I12) < 1E-15) I12 = 0.0;
+
+    long double I23 = I2 - I3;
+    if (fabsl(I23) < 1E-15) I23 = 0.0;
+
+    return log2l(fabsl(I12 / I23));
 }
